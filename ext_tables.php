b4df58ad6de0dc53c7ab7f1cfa6c725539443244
Daniel Frei <df@gate40.net>
2012-04-13 10:38:50 +0200

    initial version all files

---
+++ ext_tables.php
@@ -0,0 +1,122 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+Tx_Extbase_Utility_Extension::registerPlugin(
+	$_EXTKEY,
+	'Cloudfile',
+	'Cloud File Manager'
+);
+
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Cloud File Manager');
+
+			t3lib_extMgm::addLLrefForTCAdescr('tx_g40cloudfile_domain_model_event', 'EXT:g40cloudfile/Resources/Private/Language/locallang_csh_tx_g40cloudfile_domain_model_event.xml');
+			t3lib_extMgm::allowTableOnStandardPages('tx_g40cloudfile_domain_model_event');
+			$TCA['tx_g40cloudfile_domain_model_event'] = array(
+				'ctrl' => array(
+					'title'	=> 'LLL:EXT:g40cloudfile/Resources/Private/Language/locallang_db.xml:tx_g40cloudfile_domain_model_event',
+					'label' => 'name',
+					'tstamp' => 'tstamp',
+					'crdate' => 'crdate',
+					'cruser_id' => 'cruser_id',
+					'dividers2tabs' => TRUE,
+					'versioningWS' => 2,
+					'versioning_followPages' => TRUE,
+					'origUid' => 't3_origuid',
+					'languageField' => 'sys_language_uid',
+					'transOrigPointerField' => 'l10n_parent',
+					'transOrigDiffSourceField' => 'l10n_diffsource',
+					'delete' => 'deleted',
+					'enablecolumns' => array(
+						'disabled' => 'hidden',
+						'starttime' => 'starttime',
+						'endtime' => 'endtime',
+					),
+					'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Event.php',
+					'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_g40cloudfile_domain_model_event.gif'
+				),
+			);
+
+			t3lib_extMgm::addLLrefForTCAdescr('tx_g40cloudfile_domain_model_category', 'EXT:g40cloudfile/Resources/Private/Language/locallang_csh_tx_g40cloudfile_domain_model_category.xml');
+			t3lib_extMgm::allowTableOnStandardPages('tx_g40cloudfile_domain_model_category');
+			$TCA['tx_g40cloudfile_domain_model_category'] = array(
+				'ctrl' => array(
+					'title'	=> 'LLL:EXT:g40cloudfile/Resources/Private/Language/locallang_db.xml:tx_g40cloudfile_domain_model_category',
+					'label' => 'name',
+					'tstamp' => 'tstamp',
+					'crdate' => 'crdate',
+					'cruser_id' => 'cruser_id',
+					'dividers2tabs' => TRUE,
+					'versioningWS' => 2,
+					'versioning_followPages' => TRUE,
+					'origUid' => 't3_origuid',
+					'languageField' => 'sys_language_uid',
+					'transOrigPointerField' => 'l10n_parent',
+					'transOrigDiffSourceField' => 'l10n_diffsource',
+					'delete' => 'deleted',
+					'enablecolumns' => array(
+						'disabled' => 'hidden',
+						'starttime' => 'starttime',
+						'endtime' => 'endtime',
+					),
+					'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Category.php',
+					'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_g40cloudfile_domain_model_category.gif'
+				),
+			);
+
+			t3lib_extMgm::addLLrefForTCAdescr('tx_g40cloudfile_domain_model_bucket', 'EXT:g40cloudfile/Resources/Private/Language/locallang_csh_tx_g40cloudfile_domain_model_bucket.xml');
+			t3lib_extMgm::allowTableOnStandardPages('tx_g40cloudfile_domain_model_bucket');
+			$TCA['tx_g40cloudfile_domain_model_bucket'] = array(
+				'ctrl' => array(
+					'title'	=> 'LLL:EXT:g40cloudfile/Resources/Private/Language/locallang_db.xml:tx_g40cloudfile_domain_model_bucket',
+					'label' => 'name',
+					'tstamp' => 'tstamp',
+					'crdate' => 'crdate',
+					'cruser_id' => 'cruser_id',
+					'dividers2tabs' => TRUE,
+					'versioningWS' => 2,
+					'versioning_followPages' => TRUE,
+					'origUid' => 't3_origuid',
+					'languageField' => 'sys_language_uid',
+					'transOrigPointerField' => 'l10n_parent',
+					'transOrigDiffSourceField' => 'l10n_diffsource',
+					'delete' => 'deleted',
+					'enablecolumns' => array(
+						'disabled' => 'hidden',
+						'starttime' => 'starttime',
+						'endtime' => 'endtime',
+					),
+					'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/Bucket.php',
+					'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_g40cloudfile_domain_model_bucket.gif'
+				),
+			);
+
+			t3lib_extMgm::addLLrefForTCAdescr('tx_g40cloudfile_domain_model_file', 'EXT:g40cloudfile/Resources/Private/Language/locallang_csh_tx_g40cloudfile_domain_model_file.xml');
+			t3lib_extMgm::allowTableOnStandardPages('tx_g40cloudfile_domain_model_file');
+			$TCA['tx_g40cloudfile_domain_model_file'] = array(
+				'ctrl' => array(
+					'title'	=> 'LLL:EXT:g40cloudfile/Resources/Private/Language/locallang_db.xml:tx_g40cloudfile_domain_model_file',
+					'label' => 'filename',
+					'tstamp' => 'tstamp',
+					'crdate' => 'crdate',
+					'cruser_id' => 'cruser_id',
+					'dividers2tabs' => TRUE,
+					'versioningWS' => 2,
+					'versioning_followPages' => TRUE,
+					'origUid' => 't3_origuid',
+					'languageField' => 'sys_language_uid',
+					'transOrigPointerField' => 'l10n_parent',
+					'transOrigDiffSourceField' => 'l10n_diffsource',
+					'delete' => 'deleted',
+					'enablecolumns' => array(
+						'disabled' => 'hidden',
+						'starttime' => 'starttime',
+						'endtime' => 'endtime',
+					),
+					'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/File.php',
+					'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_g40cloudfile_domain_model_file.gif'
+				),
+			);
+
+?>
---
+++ ext_tables.sql
@@ -0,0 +1,209 @@
+#
+# Table structure for table 'tx_g40cloudfile_domain_model_event'
+#
+CREATE TABLE tx_g40cloudfile_domain_model_event (
+
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+
+	name varchar(255) DEFAULT '' NOT NULL,
+	project_nr varchar(255) DEFAULT '' NOT NULL,
+	description varchar(255) DEFAULT '' NOT NULL,
+	buckets int(11) unsigned DEFAULT '0' NOT NULL,
+	categories int(11) unsigned DEFAULT '0' NOT NULL,
+
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
+	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	starttime int(11) unsigned DEFAULT '0' NOT NULL,
+	endtime int(11) unsigned DEFAULT '0' NOT NULL,
+
+	t3ver_oid int(11) DEFAULT '0' NOT NULL,
+	t3ver_id int(11) DEFAULT '0' NOT NULL,
+	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+	t3ver_label varchar(255) DEFAULT '' NOT NULL,
+	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+	t3ver_stage int(11) DEFAULT '0' NOT NULL,
+	t3ver_count int(11) DEFAULT '0' NOT NULL,
+	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+
+	t3_origuid int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumblob,
+
+	PRIMARY KEY (uid),
+	KEY parent (pid),
+	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
+	KEY language (l10n_parent,sys_language_uid)
+
+);
+
+#
+# Table structure for table 'tx_g40cloudfile_domain_model_category'
+#
+CREATE TABLE tx_g40cloudfile_domain_model_category (
+
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+
+	event int(11) unsigned DEFAULT '0' NOT NULL,
+
+	name varchar(255) DEFAULT '' NOT NULL,
+	sort int(11) DEFAULT '0' NOT NULL,
+	label1 varchar(255) DEFAULT '' NOT NULL,
+	label2 varchar(255) DEFAULT '' NOT NULL,
+	label3 varchar(255) DEFAULT '' NOT NULL,
+
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
+	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	starttime int(11) unsigned DEFAULT '0' NOT NULL,
+	endtime int(11) unsigned DEFAULT '0' NOT NULL,
+
+	t3ver_oid int(11) DEFAULT '0' NOT NULL,
+	t3ver_id int(11) DEFAULT '0' NOT NULL,
+	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+	t3ver_label varchar(255) DEFAULT '' NOT NULL,
+	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+	t3ver_stage int(11) DEFAULT '0' NOT NULL,
+	t3ver_count int(11) DEFAULT '0' NOT NULL,
+	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+
+	t3_origuid int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumblob,
+
+	PRIMARY KEY (uid),
+	KEY parent (pid),
+	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
+	KEY language (l10n_parent,sys_language_uid)
+
+);
+
+#
+# Table structure for table 'tx_g40cloudfile_domain_model_bucket'
+#
+CREATE TABLE tx_g40cloudfile_domain_model_bucket (
+
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+
+	event int(11) unsigned DEFAULT '0' NOT NULL,
+
+	name varchar(255) DEFAULT '' NOT NULL,
+	description varchar(255) DEFAULT '' NOT NULL,
+
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
+	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	starttime int(11) unsigned DEFAULT '0' NOT NULL,
+	endtime int(11) unsigned DEFAULT '0' NOT NULL,
+
+	t3ver_oid int(11) DEFAULT '0' NOT NULL,
+	t3ver_id int(11) DEFAULT '0' NOT NULL,
+	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+	t3ver_label varchar(255) DEFAULT '' NOT NULL,
+	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+	t3ver_stage int(11) DEFAULT '0' NOT NULL,
+	t3ver_count int(11) DEFAULT '0' NOT NULL,
+	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+
+	t3_origuid int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumblob,
+
+	PRIMARY KEY (uid),
+	KEY parent (pid),
+	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
+	KEY language (l10n_parent,sys_language_uid)
+
+);
+
+#
+# Table structure for table 'tx_g40cloudfile_domain_model_file'
+#
+CREATE TABLE tx_g40cloudfile_domain_model_file (
+
+	uid int(11) NOT NULL auto_increment,
+	pid int(11) DEFAULT '0' NOT NULL,
+
+	filename varchar(255) DEFAULT '' NOT NULL,
+	description varchar(255) DEFAULT '' NOT NULL,
+	text1 varchar(255) DEFAULT '' NOT NULL,
+	text2 varchar(255) DEFAULT '' NOT NULL,
+	text3 varchar(255) DEFAULT '' NOT NULL,
+	file_category int(11) unsigned DEFAULT '0',
+	file_bucket int(11) unsigned DEFAULT '0' NOT NULL,
+
+	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
+	crdate int(11) unsigned DEFAULT '0' NOT NULL,
+	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
+	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	starttime int(11) unsigned DEFAULT '0' NOT NULL,
+	endtime int(11) unsigned DEFAULT '0' NOT NULL,
+
+	t3ver_oid int(11) DEFAULT '0' NOT NULL,
+	t3ver_id int(11) DEFAULT '0' NOT NULL,
+	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+	t3ver_label varchar(255) DEFAULT '' NOT NULL,
+	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
+	t3ver_stage int(11) DEFAULT '0' NOT NULL,
+	t3ver_count int(11) DEFAULT '0' NOT NULL,
+	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
+
+	t3_origuid int(11) DEFAULT '0' NOT NULL,
+	sys_language_uid int(11) DEFAULT '0' NOT NULL,
+	l10n_parent int(11) DEFAULT '0' NOT NULL,
+	l10n_diffsource mediumblob,
+
+	PRIMARY KEY (uid),
+	KEY parent (pid),
+	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
+	KEY language (l10n_parent,sys_language_uid)
+
+);
+
+#
+# Table structure for table 'tx_g40cloudfile_domain_model_bucket'
+#
+CREATE TABLE tx_g40cloudfile_domain_model_bucket (
+
+	event  int(11) unsigned DEFAULT '0' NOT NULL,
+
+);
+
+#
+# Table structure for table 'tx_g40cloudfile_domain_model_category'
+#
+CREATE TABLE tx_g40cloudfile_domain_model_category (
+
+	event  int(11) unsigned DEFAULT '0' NOT NULL,
+
+);
+
+#
+# Table structure for table 'tx_g40cloudfile_file_bucket_mm'
+#
+CREATE TABLE tx_g40cloudfile_file_bucket_mm (
+	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
+	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting int(11) unsigned DEFAULT '0' NOT NULL,
+	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+
+	KEY uid_local (uid_local),
+	KEY uid_foreign (uid_foreign)
+);
